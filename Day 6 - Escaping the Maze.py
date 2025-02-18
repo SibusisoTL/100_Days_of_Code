2e7cd8be5a1ae0ce6506ba89958077d1c0655ec3
Sibusiso <Sibusisoluthuli466@gmail.com>
2025-02-18 08:34:23 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ Day 6 - Escaping the Maze.py
@@ -0,0 +1,22 @@
+from random import random
+import string
+lowercase_letters = list(string.ascii_lowercase)
+uppercase_letters = list(string.ascii_uppercase)
+
+letters = lowercase_letters + uppercase_letters
+numbers = list(range(0,10))
+symbols = ['!','#','$','%','&','(',')','*','+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n "))
+nr_symbols = int(input(f"How many symbols would you like?\n "))
+nr_numbers = int(input(f"How many numbers would you like?\n "))
+
+password = ""
+for  char in range(0,nr_letters):
+    password += random.choice(letters)
+for char in range(0, nr_symbols):
+    password += random.choice(symbols)
+for char in range(0, nr_numbers):
+    password += random.choice(numbers)
+print(password)
