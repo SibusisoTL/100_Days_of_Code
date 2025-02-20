5d869a7f8aa7d6b067f21884bf92c4a46ddc1966
Sibusiso <Sibusisoluthuli466@gmail.com>
2025-02-20 12:03:12 +0200

    Merge remote-tracking branch 'origin/main'

--- Day 5 - Password Generator.py
+++ Day 5 - Password Generator.py
@@ -27,7 +27,6 @@ for number in range(1,101):
     else:
         print(number)
 
-
 #bUILDING A PASSWORD g
 
 #bUILDING A PASSWORD GENERATOR\
--- Day 6 - Escaping the Maze.py
+++ Day 6 - Escaping the Maze.py
@@ -2,8 +2,6 @@ def turn_rdef turn_right():
     turn_left()
     turn_left()
 
-
-
 while not at_goal():
     if right_is_clear():
         turn_right()
