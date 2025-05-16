587252e8db437906c2da85041db9a77d645930b9
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2025-05-16 11:46:50 +0200

    Update Treasure Hunt Game.py

--- Treasure Hunt Game.py
+++ Treasure Hunt Game.py
@@ -1,6 +1,5 @@
 print("Welcome to Treasure Island. Your mission is to find the treasure.")
 
-
 y = input("Do you want to go Left ot Right? ")
 if y == "right" or y == "Right":
     print("Fall into a hole. Game Over.")
