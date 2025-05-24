84b7e8ead0f9d440ae39e0607dc822ae857d020e
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2025-05-24 15:19:46 +0200

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -7,7 +7,6 @@ elif y == "left" or y == "Left" :
     if z == "swim" or z == "Swim":
 
 
-
         print("Attacked by trout. Game Over.")
     elif z == "Wait" or z == "wait":
         a = input("Which door, Blue, Yellow or Red? ")
