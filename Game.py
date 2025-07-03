e434764f0a7472d797ef10b59f73e7c2d00012ec
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2025-07-03 12:40:48 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -18,7 +18,6 @@ print("computer choice:")
 print(game_images[computer_choice])
 
 
-
 if user_choice >=3 or user_choice < 0:
     print("You type and invalid number. You Lose!")
 elif user_choice == 0 and computer_choice ==2:
