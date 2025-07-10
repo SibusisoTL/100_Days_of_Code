ce474484d11da999e7db8e91c90554d9746042ce
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2025-07-10 09:21:35 +0200

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -18,10 +18,6 @@ elif y == "left" or y == "Left" :
             print("Game Over.")
     else:
         
-
-
-        
-        
         print("Attacked by trout. Game Over.")
 else:
     print("Fall into a hole. Game Over."
