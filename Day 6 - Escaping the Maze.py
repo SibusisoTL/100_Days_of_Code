266705f9f3a275b72c38de2d6388ffc3945305bc
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2025-07-15 08:13:37 +0200

    Update Day 6 - Escaping the Maze.py

--- Day 6 - Escaping the Maze.py
+++ Day 6 - Escaping the Maze.py
@@ -3,7 +3,6 @@ import string
 lowercase_letters = list(string.ascii_lowercase)
 uppercase_letters = list(string.ascii_uppercase)
 
-
 letters = lowercase_letters + uppercase_letters
 numbers = list(range(0,10))
 symbols = ['!','#','$','%','&','(',')','*','+']
