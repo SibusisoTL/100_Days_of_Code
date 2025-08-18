8f0ef12d12c531fcc3b240c94723c1df153c3e2d
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2025-08-18 09:07:02 +0200

    Update Day 6 - Escaping the Maze.py

--- Day 6 - Escaping the Maze.py
+++ Day 6 - Escaping the Maze.py
@@ -1,4 +1,4 @@
-from random import random
+ from random import random
 import string
 lowercase_letters = list(string.ascii_lowercase)
 uppercase_letters = list(string.ascii_uppercase)
@@ -15,7 +15,7 @@ nr_numbers = int(input(f"How many numbers would you like?\n "))
          
 password = ""     
 for  char in range(0,nr_letters):    
-    password += random.choice(letters) 
+    password += random.choice(letters)  
 for char in range(0, nr_symbols):
     password += random.choice(symbols)
 for char in range(0, nr_numbers):
