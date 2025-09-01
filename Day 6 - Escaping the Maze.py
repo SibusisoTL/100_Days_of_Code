9a40fd2046142715e2a9525dd402d60014afaa73
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2025-09-01 11:43:02 +0200

    Update Day 6 - Escaping the Maze.py

--- Day 6 - Escaping the Maze.py
+++ Day 6 - Escaping the Maze.py
@@ -1,6 +1,6 @@
  from random import random
 import string
-lowercase_letters = list(string.ascii_lowercase)
+lowercase_letters = list(string.ascii_lowercase) 
 uppercase_letters = list(string.ascii_uppercase)
    
 letters = lowercase_letters + uppercase_letters 
