af60b9b64572ee4ccd3696631e833a6693a916de
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2025-10-24 10:25:31 +0200

    Update Day 6 - Escaping the Maze.py

--- Day 6 - Escaping the Maze.py
+++ Day 6 - Escaping the Maze.py
@@ -7,7 +7,7 @@ letters = lowercase_letters + uppercase_letters
 numbers = list(range(0,10)) 
 symbols = ['!','#','$','%','&','(',')','*','+']   
    
-print("Welcome to the PyPassword Generator!")
+print("Welcome to the PyPassword Generator!") 
 nr_letters = int(input("How many letters would you like in your password?\n "))
 nr_symbols = int(input(f"How many symbols would you like?\n ")) 
 nr_numbers = int(input(f"How many numbers would you like?\n ")) 
