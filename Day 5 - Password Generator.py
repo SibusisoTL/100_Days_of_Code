81537104c23cc719dfd289bee8fb1c316dd7f5c8
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2025-11-12 09:41:20 +0200

    Update Day 5 - Password Generator.py

--- Day 5 - Password Generator.py
+++ Day 5 - Password Generator.py
@@ -6,7 +6,7 @@ from random import random
 Student_Scores = [10, 142, 120,171, 184, 149, 24, 9, 8, 199, 78,89,86]    
 max_score = 0   
 for score in Student_Scores:                 
-    if score > max_score:             
+    if score > max_score:              
         max_score=score            
 print(max_score)                       
                                        
