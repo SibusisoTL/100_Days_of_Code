4b6e5455b120ca5d9c144bfec10e75df1aede7a9
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2025-12-04 12:02:37 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -22,7 +22,7 @@ if user_choice >=3 or user_choice < 0:
     print("You type and invalid number. You Lose!")
 elif user_choice == 0 and computer_choice ==2:
     print("You win!")
-elif computer_choice ==0 and user_choice ==2:
+elif computer_choice ==0 and user_choice ==2: 
     print("You Lose!")
 elif computer_choice> user_choice:
     print("You Lose!")
