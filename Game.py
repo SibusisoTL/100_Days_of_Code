61175226dcd16e995299502d3f11bb229818da70
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2026-01-08 08:06:44 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -13,7 +13,7 @@ game_images = [rock, paper, scissors]
 user_choice = int(input("what do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor "))
 if user_choice >= 0 and user_choice <= 2: 
     print(game_images[user_choice]
-computer_choice = random.randint(a:0,b:2)   
+computer_choice = random.randint(a:0,b:2)    
 print("computer choice:")  
 print(game_images[computer_choice])  
   
