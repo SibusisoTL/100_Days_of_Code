99c37aac4ff819082aa3ce9fc1224a0b196ef32e
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2026-01-28 07:59:38 +0200

    Update Day 5 - Password Generator.py

--- Day 5 - Password Generator.py
+++ Day 5 - Password Generator.py
@@ -1,6 +1,6 @@
    #For Loop
 import random   
-import string  
+import string   
 from random import random        
                     
 Student_Scores = [10, 142, 120,171, 184, 149, 24, 9, 8, 199, 78,89,86]       
