d51c7a9be29eb2b5a4f5fc4030fbf9cfd4b7805f
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2026-01-29 07:56:27 +0200

    Update Day 5 - Password Generator.py

--- Day 5 - Password Generator.py
+++ Day 5 - Password Generator.py
@@ -9,7 +9,7 @@ for score in Student_Scores:
     if score > max_score:                            
         max_score=score                           
 print(max_score)                                          
-                                                 
+                                                   
 ========== #for loops and Range Starts here  =========                                        
 #For Loops and Range function             
 x=0         
