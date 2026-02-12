645322d8e6c697e927ca596b63923b2babb251ef
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2026-02-12 09:10:23 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -2,7 +2,7 @@
 
 import random
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
-print(random.choice(friends))  
+print(random.choice(friends))   
 #OPTION 2    
 from random import randint       
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]   
