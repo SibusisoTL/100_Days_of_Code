5de01ea2cec1f0f28540ad33dc693d01f990ff00
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2026-02-24 13:15:05 +0200

    Update Day 5 - Password Generator.py

--- Day 5 - Password Generator.py
+++ Day 5 - Password Generator.py
@@ -1,7 +1,7 @@
 #For Loop  
 import random     
 import string          
-from random import random                  
+from random import random                   
      
 Student_Scores = [10, 142, 120,171, 184, 149, 24, 9, 8, 199, 78,89,86]          
 max_score = 0          
@@ -11,7 +11,7 @@ for score in Student_Scores:
         max_score=score                                            
 print(max_score)                                                      
                                                        
-========== #for loops and Range Starts here  =========                                          
+========== #for loops and Range Starts here  =========                                           
 #For Loops and Range function                 
 x=0         
 for number in range(1,101):        
