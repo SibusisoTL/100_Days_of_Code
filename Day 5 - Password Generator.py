f0e1ed8f37012616071044e407cbbc62e04178dc
Sibusiso Luthuli <60149779+SibusisoTL@users.noreply.github.com>
2026-03-26 12:26:27 +0200

    Update Day 5 - Password Generator.py

--- Day 5 - Password Generator.py
+++ Day 5 - Password Generator.py
@@ -20,7 +20,7 @@ print(x)
   
   
 #Task  
-for number in range(1,101): 
+for number in range(1,101):  
     if (number % 3 == 0) and (number % 5 == 0): 
         print("FizzBuzz") 
     elif number%3 == 0: 
